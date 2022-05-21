8adb51a1e8a75d603266acb1adda8527ec1b33ad
limi96 <mikelee199607@gmail.com>
2022-05-21 16:38:34 +0300

    InputCode refactored and typecheck for int arguments

--- lexer.py
+++ lexer.py
@@ -1,6 +1,6 @@
 class Lexer: 
-    def __init__(self, inputcode):
-        self.inputcode = inputcode.lower()
+    def __init__(self, input_code):
+        self.input_code = input_code.lower()
     
     KEYWORDS = ['eteen', 'taakse', 'oikealle', 'vasemmalle']
     DIGITS = '0123456789'
@@ -8,37 +8,30 @@ class Lexer:
     def create_tokens(self):
         token_list = []
 
-        print("inputcode",self.inputcode)
+        print("inputcode",self.input_code)
         
-        split_list = [element.lower() for element in self.inputcode.split()]
+        split_list = [element.lower() for element in self.input_code.split()]
         print("Split:", split_list)
         
         for element in split_list:
             if element in self.KEYWORDS:
                 token_list.append(("KEYWORD",element))
             elif element in self.DIGITS:
-                token_list.append(("INT", element))
+                token_list.append(("INT", int(element)))
             else:
                 print("NOT FOUND ", element)
         print("TOKENS:",token_list)
         
-<<<<<<< HEAD
-        return token_list
-        
-    def set_input(self, input): 
-        self.input = input.lower()
-=======
-    def set_inputcode(self, inputcode): 
-        self.inputcode = inputcode.lower()
+    def set_input_code(self, input_code): 
+        self.input_code = input_code.lower()
     
->>>>>>> 933aaab2f051010f3c1abb98d6223ea622e0758e
 
 """
-inputcode = "Eteen 5 Taakse 5"
-lex = Lexer(inputcode)
+input_code = "Eteen 5 Taakse 5"
+lex = Lexer(input_code)
 lex.create_tokens()
 
-inputcode = "Eteen 5 Taakse5"
-lex.set_inputcode(inputcode)
+input_code = "Eteen 5 Taakse5"
+lex.set_input_code(input_code)
 lex.create_tokens()
 """
