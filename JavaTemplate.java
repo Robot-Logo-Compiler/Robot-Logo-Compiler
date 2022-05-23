e1b9235748b69a5eaee8102060e216d805d5245d
Heikki Venhola <hede@heikkivenhola.fi>
2022-05-23 10:50:23 +0300

    add template for generated code

---
+++ JavaTemplate.java
@@ -0,0 +1,55 @@
+
+import lejos.nxt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        init();
+
+        // insert generated code here
+
+
+    }
+
+    private static void init(int speed) {
+
+        Motor.A.setSpeed(speed);
+        Motor.B.setSpeed(speed);
+    }
+
+    private static void init() {
+
+        Motor.A.setSpeed(100);
+        Motor.B.setSpeed(100);
+
+    }
+
+    private static void moveForward(int amount) {
+
+        Motor.A.forward();
+        Motor.B.forward();
+        Thread.sleep(500 * amount);
+        Motor.A.stop();
+        Motor.B.stop();
+
+    }
+
+    private static void moveBackward(int amount) {
+
+        Motor.A.backward();
+        Motor.B.backward();
+        Thread.sleep(500 * amount);
+        Motor.A.stop();
+        Motor.B.stop();
+        
+    }
+
+    private static void rotate(int angle) {
+
+        Motor.A.rotate(angle);
+        Motor.B.rotate(-1 * angle)
+    }
+
+
+}
