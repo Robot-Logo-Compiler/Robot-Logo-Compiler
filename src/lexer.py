804f605ae16078b0b3eeb35220f23001e854e204
Heikki Venhola <hede@heikkivenhola.fi>
2022-06-08 22:02:00 +0300

    pylint fixes

--- src/lexer.py
+++ src/lexer.py
@@ -1,11 +1,19 @@
+'''This module splits Logo commands into a list of tokens for the parser'''
+
 from src.logo_keywords import LOGO_KEYWORDS
-from src.error_handler import LexerError
+#from src.error_handler import LexerError
+
 class Lexer:
+    ''' This class splits a given Logo code by commands and compares them with a constant keyword list.
+    Finally, commands are turned into a list of tokens. '''
+
     def __init__(self, input_code):
         self.input_code = input_code
         self.KEYWORDS = LOGO_KEYWORDS
 
     def create_tokens(self):
+        ''' Goes through every word in code. If they are found in KEYWORDS, they are interpreted as
+        commands, or otherwise parameters. '''
         token_list = []
 
         #print("inputcode",self.input_code)
@@ -25,4 +33,5 @@ class Lexer:
         return token_list
 
     def set_input_code(self, input_code):
+        ''' A setter function for input code. '''
         self.input_code = input_code
