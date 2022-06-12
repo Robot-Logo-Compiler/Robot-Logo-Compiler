b5c3a6914acd314ee33bcc843c56f80b593c620e
limi96 <mikelee199607@gmail.com>
2022-06-12 12:21:49 +0300

    Improved pylint in error_handler and lexer: White space removal/addition and docstrings

--- src/error_handler.py
+++ src/error_handler.py
@@ -1,11 +1,22 @@
-import sys, inspect
+# import sys
+# import inspect
+'''
+As the name suggests, this file handels all of the error messages that the compiler raises.
+When SystemExit(0) is called via raise_system_exit(), no tracebacks will be shown and the compiler will stop executing.
+The terminal message will only consist of the custom compiler error message specified in this script. 
+'''
 
+'''
+Used for toggling traceback error messages on and off.
+'''
 def raise_system_exit():
     raise SystemExit(0)
     # pass
 
+'''
+Error handler for file input in compiler.py
+'''
 class InputFileError():
-
     #   These messages are only in English, since they are only for the teacher to see
     def too_many_arguments():
         print('Too many arguments. Please provide one file of Logo code as argument.')
@@ -19,19 +30,23 @@ class InputFileError():
         print('Could not open file:', file, 'Please provide one file of Logo code as argument.')
         raise_system_exit()
 
+'''
+Error handler for lexer.py
+'''
 class LexerError():
     def unknown_command(command):
         print('En tunnistanut komentoa: "', command, '". Tarkista, että komento on olemassa ja että se on varmasti kirjoitettu oikein :)')
         # print('I could not recognize the command: ', command, '. Please check that the command is spelled correctly :)')
         raise_system_exit()
 
+'''
+Error handler for parser.py
+'''
 class ParserError():
     def parameter_without_command(node):
         print(f"Annoit komennoksi {node.value} joka ei ole komentosana. Unohtuiko sinulta komento?")
         
-
     def child_is_invalid_type(keyword, input_child, correct_type, invalid_type):
-
         print('Komento "', keyword, '" haluaa syötteeksi "', correct_type,
                 '" mutta sen sijaan komento sai syötteeksi "', invalid_type, '".' )
         print('Pystyisitkö vaihtamaan syötteen "', input_child.return_type(), '" tilalle oikeanlaisen syötteen?')
@@ -40,7 +55,6 @@ class ParserError():
         #        '" but instead the command received an input of "', invalid_type, '".' )
         # print('Would you please change the input "', input_child, '" into a correct one?')
         raise_system_exit()
-
 
 
 class SemanticException(BaseException):
@@ -59,9 +73,7 @@ class InvalidChildTypeException(SemanticException):
         Pystyisitkö vaihtamaan syötteen {parameter} tilalle oikeanlaisen syötteen?"""
         super().__init__(keyword, self.message)
 
-
 class FileException():
-
     def wrong_number_of_files(amount):
         if amount == 0:
             print("Et antanut tiedostoa käännettäväksi.")
@@ -70,3 +82,4 @@ class FileException():
 
     def os_not_able_to_open_file(path):
         print(f"Käyttöjärjestelmä ei pystynyt avaamaan tiedostoa sijainnissa {path}?")
+    
--- src/lexer.py
+++ src/lexer.py
@@ -9,7 +9,7 @@ class Lexer:
 
     def __init__(self, input_code):
         self.input_code = input_code
-        self.KEYWORDS = LOGO_KEYWORDS
+        self.keywords = LOGO_KEYWORDS
 
     def create_tokens(self):
         ''' Goes through every word in code. If they are found in KEYWORDS, they are interpreted as
@@ -20,10 +20,9 @@ class Lexer:
 
         split_list = self.input_code.replace('\n', ' ').replace('(', ' ( ').replace(')', ' ) ').split()
         # print("Split:", split_list)
-        
         #previous = ''
         for element in split_list:
-            if element.lower() in self.KEYWORDS.keys():
+            if element.lower() in self.keywords.keys():
                 token_list.append(("KEYWORD",element.lower()))
             else:
                 token_list.append(("PARAMETER", element))
@@ -34,4 +33,4 @@ class Lexer:
     def set_input_code(self, input_code):
         ''' A setter function for input code. '''
         self.input_code = input_code
-
+        
