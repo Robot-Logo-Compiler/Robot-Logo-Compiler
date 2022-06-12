9137c6c76fc1749eb82b954df6449679e936d733
JuhahuJ <juha.lehtinen@helsinki.fi>
2022-06-12 20:34:49 +0300

    add english language test to generator

--- src/tests/test_generator.py
+++ src/tests/test_generator.py
@@ -1,15 +1,15 @@
+from turtle import forward
 import unittest
 from src.code_generator import Generator
 
 class StubTree:
-    def __init__(self):
-        self.root = StubRoot()
-        #print(self.root.children)
+    def __init__(self, parameter_list):
+        self.root = StubRoot(parameter_list)
 
 class StubRoot:
-    def __init__(self):
+    def __init__(self, parameter_list):
         self.children = []
-        keyword_parameter_list = [('tulosta','"moikka'),('eteen','5'),('taakse','4'),('vasemmalle','7'),('oikealle','9')]
+        keyword_parameter_list = parameter_list
         for pair in keyword_parameter_list:
             self.children.append(StubKeywordNode(pair[0], pair[1]))
 
@@ -24,11 +24,15 @@ class StubParameterNode:
         self.value = parameter
         
 
-class TestGenerator(unittest.TestCase): # Tämä testiluokka on väliaikainen ja on tarkoitus korvata testeillä koodigeneraattorille
+class TestGenerator(unittest.TestCase):
     def setUp(self):
-        self.generator = Generator(StubTree())
+        self.generator = Generator(StubTree([('tulosta','"moikka'),('eteen','5'),('taakse','4'),('vasemmalle','7'),('oikealle','9')]))
+        self.generator2 = Generator(StubTree([('forward','4'),('left','55'),('show','"vaarakieli'),('back','333'),('right','1')]))
 
-    def test_generator_creates_correct_command_list(self):
+    def test_generator_creates_correct_command_list_finnish(self):
         self.generator.list_commands()
-        print(self.generator.command_list)
         self.assertEqual(['printToLCD("moikka")','travel(5)','travel(-4)','rotate(-7)','rotate(9)'],self.generator.command_list)
+
+    def test_generator_creates_correct_command_list_english(self):
+        self.generator2.list_commands()
+        self.assertEqual(['travel(4)','rotate(-55)','printToLCD("vaarakieli")','travel(-333)','rotate(1)'],self.generator2.command_list)
