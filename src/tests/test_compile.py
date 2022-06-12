2d9b1ce24353ae20e86f1e5bbfc04782cd0dd8db
Antti <secnec@pm.me>
2022-06-12 21:37:16 +0300

    Refactored tests for main program

--- src/tests/test_compile.py
+++ src/tests/test_compile.py
@@ -1,24 +1,21 @@
 import unittest
 import sys, io
-from unittest.mock import MagicMock, PropertyMock
 from compile import openfile
-import src.error_handler
 
 class testCompile(unittest.TestCase):
+    def setUp(self):
+        self.capturedOutput = io.StringIO() 
+        sys.stdout = self.capturedOutput
+
+    def tearDown(self):
+        sys.stdout = sys.__stdout__
 
     def test_no_file_provided(self):
-        capturedOutput = io.StringIO() 
-        sys.stdout = capturedOutput
         openfile([])
-        sys.stdout = sys.__stdout__
-        self.assertIn("Et antanut tiedostoa käännettäväksi.", capturedOutput.getvalue())
+        self.assertIn("Et antanut tiedostoa käännettäväksi.", self.capturedOutput.getvalue())
 
-    
     def test_too_many_files_provided(self):
-        capturedOutput = io.StringIO() 
-        sys.stdout = capturedOutput
         openfile(["file1", "file2"])
-        sys.stdout = sys.__stdout__
-        self.assertIn("Annoit liian monta tiedostoa käännettäväksi.", capturedOutput.getvalue())
+        self.assertIn("Annoit liian monta tiedostoa käännettäväksi.", self.capturedOutput.getvalue())
 
 
