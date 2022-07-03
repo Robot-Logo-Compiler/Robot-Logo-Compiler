abe9e3360d3734b3ed8ac1b2f1163ee95bfbb623
JuhahuJ <juha.lehtinen@helsinki.fi>
2022-07-03 21:44:04 +0300

    add docstring to analyzer

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,14 +1,24 @@
+"""This module is the semantic analyzer"""
 from src.error_handler import SemanticException
 
 class Analyzer:
-    def __init__(self, tree):
-        self.type_check(tree.root.children)
+    """
+    This is the analyzer class.
+    It goes through the parser tree and checks it for errors.
+    """
 
-    def type_check(self, tree):
+    def __init__(self, tree):
+        self.go_through_children(tree.root.children)
+
+    def go_through_children(self, tree):
+        """Runs the check type function for all children in the parser tree."""
+
         for child in tree:
             self.check_type(child)
 
     def check_type(self, node):
+        """Compares nodes child's type with wanted type."""
+
         if hasattr(node, "child"):
             if self.check_type(node.child) not in node.expected_child():
                 SemanticException.child_is_invalid_type(node.keyword)
