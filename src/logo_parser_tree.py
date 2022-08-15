c9222042dbaece4e2d7e40b552924e19d3410fa5
JuhahuJ <juha.lehtinen@helsinki.fi>
2022-08-15 21:59:23 +0300

    removed unused code from parser tree

--- src/logo_parser_tree.py
+++ src/logo_parser_tree.py
@@ -41,30 +41,10 @@ class KeywordNode:
         self.child.check_types(st)
 
     def check_type(self):
-        valid_types = ["str","int","float","KeywordNode","Parameternode","Stringnode","BinaryOperationNode","VariableNode","NameVariableNode"]
+        valid_types = ["str","int","float","KeywordNode","Parameternode","BinaryOperationNode","VariableNode","NameVariableNode"]
         if self.child.check_type() not in valid_types:
             SemanticException.child_is_invalid_type(self.keyword)
         return "KeywordNode"
-        #if not isinstance(self.keyword, str):
-        #    SemanticException.child_is_invalid_type(self.keyword)
-
-        #child_is_valid_type =  isinstance(self.child, str) \
-        #                    or isinstance(self.child, int) \
-        #                    or isinstance(self.child, float) \
-        #                    or isinstance(self.child, KeywordNode) \
-        #                    or isinstance(self.child, ParameterNode) \
-        #                    or isinstance(self.child, StringNode) \
-        #                    or isinstance(self.child, BinaryOperationNode) \
-        #                    or isinstance(self.child, VariableNode) \
-        #                    or isinstance(self.child, NameVariableNode)
-
-        #if not child_is_valid_type:
-        #    SemanticException.child_is_invalid_type(self.child.__str__())
-
-
-    '''KeywordNode only accepts a parameter or a binary operation as a child'''
-    def expected_child(self):
-        return ["bin_operator", "parameter"]
 
     def __str__(self) -> str:
         return '(' + 'KeywordNode ' + ' keyword: ' + self.keyword.__str__() + ' parameter: ' + self.child.__str__() + ') ->'
@@ -77,10 +57,6 @@ class ParameterNode:
     def token_type(self):
         return "parameter"
 
-    ''' ParameterNode can not have a child, always false'''
-    def expected_child(self):
-        return []
-
     def check_type(self):
         if self.value.isnumeric():
             return "int"
@@ -88,13 +64,6 @@ class ParameterNode:
             return "float"
         else:
             return "str"
-
-        child_is_valid_type =  isinstance(self.child, str) \
-                            or isinstance(self.child, int) \
-                            or isinstance(self.child, float)
-
-        if not child_is_valid_type:
-            SemanticException.child_is_invalid_type(self.child.__str__())
 
     def check_types(self, st):
         self.get_type()
@@ -115,27 +84,6 @@ class ParameterNode:
     def __str__(self) -> str:
         return '(' + 'ParameterNode ' + ' value: ' + self.value.__str__() + ') ->'
 
-class StringNode:
-
-    def __init__(self, value=None):
-        self.value = value
-
-    def token_type(self):
-        return "string"
-
-    ''' ParameterNode can not have a child, always false'''
-    def expected_child(self):
-        return []
-
-    def check_type(self):
-
-        if not isinstance(self.value, str):
-            SemanticException.child_is_invalid_type(self.value)
-
-    def __str__(self) -> str:
-        return '(' + 'StringNode ' + ' value: ' + self.value.__str__() + ') ->'
-
-
 class BinaryOperationNode:
 
     def __init__(self, operand_type="plus", left=None, right=None):
@@ -153,14 +101,9 @@ class BinaryOperationNode:
         return "bin_operator"
         #Should return number. All binary operators are considered equivalent to numbers
 
-    ''' binary operation must have a parameter or another binary operation as a child'''
-    def expected_child(self):
-        return ["bin_operator", "parameter"]
-
     def check_types(self, st):
         self.child_one.check_types(st)
         self.child_two.check_types(st)
-        
 
     def check_type(self):
         valid_types = ["int","float","KeywordNode"]
@@ -170,11 +113,6 @@ class BinaryOperationNode:
             print("Ff3")
             SemanticException.child_is_invalid_type(self)
         return "float"
-        if isinstance(self.child_one, int) or isinstance(self.child_one, float) or isinstance(self.child_one, VariableNode):
-            SemanticException.child_is_invalid_type('BinaryOperationNode child_one ')
-
-        if isinstance(self.child_two, int) or isinstance(self.child_two, float) or isinstance(self.child_two, VariableNode):
-            SemanticException.child_is_invalid_type('BinaryOperationNode child_two ')
 
     def get_type(self):
         return "double"
@@ -191,8 +129,8 @@ class NameVariableNode:
             return symbol_table[self.name]
         return "variable"
 
-    def expected_child(self):
-        return []
+    def check_types(self, st):
+        return self.get_type(st)
 
     def check_type(self):
         if not isinstance(self.name, str):
@@ -216,9 +154,6 @@ class VariableNode:
             return symbol_table[self.name]
         return "variable"
 
-    def expected_child(self):
-        return ["string", "parameter"]
-
     def check_types(self, st):
         st[self.name.name] = self.value.get_type()
 
@@ -231,23 +166,6 @@ class VariableNode:
 
         return "VariableNode"
 
-        child_is_valid_type =  isinstance(self.name, str) \
-                            or isinstance(self.name, NameVariableNode)
-
-        if not child_is_valid_type:
-            SemanticException.child_is_invalid_type('VariableNode Name ')
-
-        child_is_valid_type =  isinstance(self.value, str) \
-                            or isinstance(self.value, int) or isinstance(self.value, float) \
-                            or isinstance(self.value, KeywordNode) \
-                            or isinstance(self.value, ParameterNode) \
-                            or isinstance(self.value, StringNode) \
-                            or isinstance(self.value, BinaryOperationNode) \
-                            or isinstance(self.value, VariableNode) \
-                            or isinstance(self.value, NameVariableNode)
-
-        if not child_is_valid_type:
-            SemanticException.child_is_invalid_type('VariableNode Value ')
     #TestVersion
     def type_check(self, symbol_table):
         if self.name not in symbol_table:
